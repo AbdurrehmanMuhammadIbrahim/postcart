26b2697b2fbf3e3718f307693f6610279b1a30b3
Abdurrehman <education24to7@gmail.com>
2024-10-25 18:41:58 +0500

    add new component in component

--- app/update-prompt/page.js
+++ app/update-prompt/page.js
@@ -1,81 +1,16 @@
-"use client";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 
-import { useState,useEffect,Suspense } from 'react'
-import { useRouter,useSearchParams } from 'next/navigation'
+const EditPrompt = dynamic(() => import("@components/EditPrompt"), {
+  suspense: true,
+});
 
-import Form from "@components/Form";
-
-const EditPrompt = () => {
- const router = useRouter();
-
- const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
-
-  const [post, setPost] = useState({ prompt: "", tag: "", });
-  const [submitting,setSubmitting] = useState(false);
-;
-
-useEffect(()=>{
-const getPromptDetails = async () => {
-    const response = await fetch(`/api/prompt/${promptId}`);
-
-    const data = await response.json();
-// console.log("promptdata",data)
-
-    setPost ({
-        prompt: data.prompt,
-        tag: data.tag,
-    })
-}
-if(promptId)getPromptDetails();
-},[promptId]);
-
-
-
-const UpdatePrompt = async (e)=>{
-e.preventDefault();
-setSubmitting(true);
-
-if (!promptId) return alert("Missing PromptId!");
-
-try {
-   const response =await fetch (`/api/prompt/${promptId}`,
-   {
-    method:"PATCH",
-    body:JSON.stringify({
-      prompt: post.prompt,
-      tag: post.tag
-    })
-   })
-
-   if(response.ok){
-    router.push("/");
-   }
-} catch (error) {
-  console.log ("Update prompt error",error);
-}finally{
-  setSubmitting(false);
-}
-
-}
-
-if (!promptId) {
-  return <div>Loading...</div>; // Early return if promptId is missing
-}
+const Page = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-
-    <div>
-      <Form
-      type="Edit"
-      post = {post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit= {UpdatePrompt}
-      />
-    </div>
+    <Suspense>
+      <EditPrompt />
     </Suspense>
-  )
-}
+  );
+};
 
-export default EditPrompt
+export default Page;
